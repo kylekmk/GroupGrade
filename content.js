b3af036bcb43534f7593631ab1f2a92d08730083
Kyle Kamka <kkamka@cs.utexas.edu>
2021-03-22 14:43:16 -0500

    Text box identification added

--- content.js
+++ content.js
@@ -1,7 +1,22 @@
 chrome.runtime.onMessage.addListener(function (request) {
-   var vids = document.getElementsByTagName("video");
-   console.log(vids);
-   console.log(document.getElementById('player0'));
-   vids[0].playbackRate = request;
+
+   var text_areas = document.getElementsByTagName('textarea');
+   var comments = new Array();
+   var i;
+   console.log(text_areas);
+
+   for (i = 0; i < text_areas.length; i++){
+      var curr_text = text_areas[i];
+
+      if (!(curr_text.id == 'criterion_comments_textarea' || curr_text.id == 'rating_form_description' || curr_text.id == 'speed_grader_comment_textarea' || curr_text.className == 'long_description' || curr_text.className == 'description')){
+         comments.push(curr_text);
+      }
+   }
+
+   console.log(comments);
+
+   for (i = 0; i < comments.length; i++){
+      comments[i].value = request;
+   }
    
-})
+});
--- popup.js
+++ popup.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    document.getElementById('btn').onclick = function() { setValue(); };
+
+    var input = document.getElementById('comments');
+
+    function setValue() {
+        text = input.value;
+        console.log('called with ' + text);
+        chrome.tabs.query({ currentWindow: true, active: true },
+            function(tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, text);
+            });
+    }
 
 }, false);
